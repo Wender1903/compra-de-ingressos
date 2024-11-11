9a0d26130c9c15343832e4e063abae0e1cc9eb35
Wender1903 <151679491+Wender1903@users.noreply.github.com>
2024-11-10 23:51:45 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,43 +1,37 @@
 function comprar() {
-    let quantidadeDisponivel = 0
-    let ingresso = document.getElementById("tipo-ingresso").value
-    let quantidade = document.getElementById("qtd").value
+    let quantidadeDisponivel = 0;
+    let ingresso = document.getElementById("tipo-ingresso").value;
+    let quantidade = parseInt(document.getElementById("qtd").value);
 
     let quantidadePista = document.getElementById("qtd-pista");
     let quantidadeSuperior = document.getElementById("qtd-superior");
     let quantidadeInferior = document.getElementById("qtd-inferior");
 
-    if (ingresso == "pista") {
-        quantidadeDisponivel = quantidadePista.textContent;
+    if (ingresso === "pista") {
+        quantidadeDisponivel = parseInt(quantidadePista.textContent);
         acoes();
-        quantidadePista.innerHTML = quantidadeDisponivel;
+        quantidadePista.textContent = quantidadeDisponivel;
 
-    }
-
-    else if (ingresso == "superior") {
-        quantidadeDisponivel = quantidadeSuperior.textContent;
+    } else if (ingresso === "superior") {
+        quantidadeDisponivel = parseInt(quantidadeSuperior.textContent);
         acoes();
-        quantidadeSuperior.innerHTML = quantidadeDisponivel;
+        quantidadeSuperior.textContent = quantidadeDisponivel;
 
-    }
-    else {
-        quantidadeDisponivel = quantidadeInferior.textContent;
+    } else {
+        quantidadeDisponivel = parseInt(quantidadeInferior.textContent);
         acoes();
-        quantidadeInferior.innerHTML = quantidadeDisponivel ;
-
+        quantidadeInferior.textContent = quantidadeDisponivel;
     }
-
-
 
     function acoes() {
-        if (quantidadeDisponivel == 0) {
+        if (quantidadeDisponivel === 0) {
             alert(`As cadeiras da parte "${ingresso}" da sala já foram alugadas, por favor, escolha outra cadeira`);
-            return ;
+            return;
         } else if (quantidade > quantidadeDisponivel) {
-            alert("A quantidade de cadeiras que você deseja alugar é maior do que temos disponivel, por favor alugue um número de cadeiras válidas");
+            alert("A quantidade de cadeiras que você deseja alugar é maior do que temos disponível, por favor alugue um número de cadeiras válido");
             return;
         } else {
-            quantidadeDisponivel = quantidadeDisponivel - quantidade
+            quantidadeDisponivel -= quantidade;
         }
     }
 }
